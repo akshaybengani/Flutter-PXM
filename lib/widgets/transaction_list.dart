8e2b1d1f893ebec3afffc4ffa3401f9074d8465c
Akshay Bengani <akshaybengani@gmail.com>
2019-12-18 03:51:16 +0530

    ChartAdded

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,59 +10,79 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      child: ListView(
-        children: transactions.map((tx) {
-          return Card(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      height: 420,
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  // decoration: BoxDecoration(
-                  //   border: Border.all(
-                  //     color: Colors.black,
-                  //     width: 2,
-                  //   ),
-                  // ),
+                  margin: EdgeInsets.symmetric(vertical: 20),
                   child: Text(
-                    '\u20B9' + tx.amount.toString(),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.green,
-                    ),
-                    textAlign: TextAlign.center,
+                    'No Transactions Added yet!',
+                    style: Theme.of(context).textTheme.title,
                   ),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5, right: 20),
-                      child: Text(
-                        DateFormat().format(tx.date),
-                        style: TextStyle(
-                          color: Colors.grey,
+                Image.asset(
+                  'assets/images/waiting.png',
+                  height: 300,
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColorDark,
+                            width: 2,
+                          ),
+                        ),
+                        child: Text(
+                          '\u20B9' +
+                              transactions[index].amount.toStringAsFixed(2),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColorDark,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            transactions[index].title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(top: 5, right: 20),
+                            child: Text(
+                              DateFormat().format(transactions[index].date),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        }).toList(),
-      ),
     );
   }
 }
